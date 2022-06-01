321e9352d629c0669c5cfb46296777779318c86d
marshallku <marshall@kakao.com>
2022-06-01 21:26:06 +0900

    Do return fragment before setting attributes

--- src/index.ts
+++ src/index.ts
@@ -11,46 +11,6 @@ export default function crtElt<
             ? document.createDocumentFragment()
             : document.createElement(nodeName);
 
-    if (!(node instanceof DocumentFragment)) {
-        Object.entries(attributes).forEach(([key, value]) => {
-            if (key === "events") {
-                Object.entries(value as Partial<CustomEvents<T>>).forEach(
-                    ([type, args]) => {
-                        if (Array.isArray(args)) {
-                            const curArgs = args as [
-                                EventListenerOrEventListenerObject,
-                                boolean | AddEventListenerOptions | undefined
-                            ];
-
-                            node.addEventListener(type, ...curArgs);
-
-                            return;
-                        }
-
-                        const curArgs =
-                            args as EventListenerOrEventListenerObject;
-
-                        node.addEventListener(type, curArgs);
-                    }
-                );
-
-                return;
-            }
-
-            if (key in node) {
-                try {
-                    node[key as "innerText"] = value as string;
-                } catch {
-                    node.setAttribute(key, value as string);
-                }
-
-                return;
-            }
-
-            node.setAttribute(key, value as string);
-        });
-    }
-
     children.forEach((childNode) => {
         if (!childNode) {
             return;
@@ -63,6 +23,47 @@ export default function crtElt<
         }
 
         node.appendChild(childNode);
+    });
+
+    if (node instanceof DocumentFragment) {
+        return node as U;
+    }
+
+    Object.entries(attributes).forEach(([key, value]) => {
+        if (key === "events") {
+            Object.entries(value as Partial<CustomEvents<T>>).forEach(
+                ([type, args]) => {
+                    if (Array.isArray(args)) {
+                        const curArgs = args as [
+                            EventListenerOrEventListenerObject,
+                            boolean | AddEventListenerOptions | undefined
+                        ];
+
+                        node.addEventListener(type, ...curArgs);
+
+                        return;
+                    }
+
+                    const curArgs = args as EventListenerOrEventListenerObject;
+
+                    node.addEventListener(type, curArgs);
+                }
+            );
+
+            return;
+        }
+
+        if (key in node) {
+            try {
+                node[key as "innerText"] = value as string;
+            } catch {
+                node.setAttribute(key, value as string);
+            }
+
+            return;
+        }
+
+        node.setAttribute(key, value as string);
     });
 
     return node as U;

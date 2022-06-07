61892483cf75608b8ba195ff5c9190717418febc
marshall-ku <marshall@kakao.com>
2022-06-08 01:33:51 +0900

    Fix typo

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default [
     {
         input: "src/index.ts",
         output: {
-            name: "Zoom",
+            name: "crtElt",
             file: pkg.main.replace(".min", ""),
             format: "umd",
         },
@@ -32,7 +32,7 @@ export default [
     {
         input: "src/index.ts",
         output: {
-            name: "Zoom",
+            name: "crtElt",
             file: pkg.main,
             format: "umd",
         },

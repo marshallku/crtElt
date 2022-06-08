bf18cce864f1a5d83c9368882af2a0567d3a9417
marshallku <marshall@kakao.com>
2022-06-08 11:52:21 +0900

    Fix className when node is SVGElement

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,12 @@ export default function crtElt<
         }
 
         if (node instanceof SVGElement) {
+            if (key === "className") {
+                node.classList.add(...value.split(" "));
+
+                return;
+            }
+
             node.setAttribute(key, value);
 
             return;
